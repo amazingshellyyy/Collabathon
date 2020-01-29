5e92a479dcc1cdd22d48d34b8d90056f88b71801
amazingshellyyy <amazingshellyyy@gmail.com>
2020-01-29 11:10:36 -0800

    full responsive with backgroung image

--- public/js/app.js
+++ public/js/app.js
@@ -37,3 +37,44 @@ $form.on('submit', ()=>{
 
 
 });
+
+const $input = $('input');
+console.log($input);
+// $input.on('keyup', ()=> {
+//     $input.addClass('filled');
+// })
+
+//setup before functions
+var typingTimer;                //timer identifier
+var doneTypingInterval = 2000;  //time in ms, 5 second for example
+// var $input = $('#myInput');
+
+//on keyup, start the countdown
+$input.on('keyup', function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+});
+
+//on keydown, clear the countdown 
+$input.on('keydown', function () {
+  clearTimeout(typingTimer);
+});
+
+//user is "finished typing," do something
+function doneTyping () {
+    $input.css('background-color','white');
+}
+
+
+// $(window).resize(()=> {
+//     const $fullbox = $('body > div');
+//     const $upperbox = $('.full-box > div').eq(0);
+//     const $lowerbox = $('.full-box > div').eq(1);
+//     const $windowWidth = window.innerWidth;
+//     console.log($windowWidth);
+//     if ($windowWidth < 768) {
+//     $upperbox.removeClass();
+//     $lowerbox.removeClass();
+//     $fullbox.addClass();
+// }
+// })
